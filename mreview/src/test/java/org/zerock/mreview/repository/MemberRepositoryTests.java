2d56be1dcb0c3a56ae3e7ac5f1b42bb9cb0c4043
rasungKi <rasung-ki@godo.co.kr>
2021-01-20 22:34:46 +0900

    회원 삭제 트랜잭션 처리

--- mreview/src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -3,8 +3,10 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.zerock.mreview.entity.Member;
 
+import javax.transaction.Transactional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -12,6 +14,9 @@ public class MemberRepositoryTests {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @Test
     public void insertMembers() {
@@ -22,5 +27,20 @@ public class MemberRepositoryTests {
                     .nickname("reviewer" + i).build();
             memberRepository.save(member);
         });
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMembers() {
+        Long mid = 7L;
+
+        Member member = Member.builder().mid(mid).build();
+        // basic
+        // memberRepository.deleteById(mid);
+        // reviewRepository.deleteByMember(member);
+
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
     }
 }
